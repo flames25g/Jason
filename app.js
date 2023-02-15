1943874a0dcb9f68d1bd19a05118d02db0286c1a
Jason Lee <flames25g@gmail.com>
2023-02-15 12:18:28 +1100

    Weather added

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-// Current Date
+// Current Date, Time
 
-var monthNames = [
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+const months = [
   "Jan",
   "Feb",
   "Mar",
@@ -14,27 +23,91 @@ var monthNames = [
   "Nov",
   "Dec",
 ];
-var n = new Date();
-var y = n.getFullYear();
-var m = n.getMonth();
-var d = n.getDate();
-document.getElementById("date").innerHTML = d + "/" + monthNames[m] + "/" + y;
 
-// Current time
-
-setInterval(function () {
-  var d = new Date();
-  var hours = d.getHours();
-  var minutes = d.getMinutes();
-  var seconds = d.getSeconds();
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HoursFormat = hour >= 13 ? hour % 12 : hour;
+  const minutes = time.getMinutes();
+  const minutesFormat = minutes <= 10 ? "0" + minutes : minutes;
+  const seconds = time.getSeconds();
+  const ampm = hour >= 12 ? "PM" : "AM";
 
   document.getElementById("time").innerHTML =
-    (hours < 10 ? "0" + hours : hours) +
+    (hoursIn12HoursFormat < 10
+      ? "0" + hoursIn12HoursFormat
+      : hoursIn12HoursFormat) +
     ":" +
-    (minutes < 10 ? "0" + minutes : minutes) +
+    minutesFormat +
     ":" +
-    (seconds < 10 ? "0" + seconds : seconds);
+    (seconds < 10 ? "0" + seconds : seconds) +
+    " " +
+    `<span id="am-pm">${ampm}</span>`;
+
+  document.getElementById("date").innerHTML =
+    days[day] + " " + date + " " + months[month] + ", ";
 }, 1000);
+
+// Weather
+
+getWeatherData();
+function getWeatherData() {
+  navigator.geolocation.getCurrentPosition((success) => {
+    console.log(success);
+
+    let { latitude, longitude } = success.coords;
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=41cd218c4549c273819aa70e8ad25629`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        showWeatherData(data);
+      });
+  });
+}
+
+const currentWeatherItemsEl = document.getElementById("current-weather-items");
+const timezone = document.getElementById("time-zone");
+const country = document.getElementById("country");
+const currentTempEl = document.getElementById("current-temp");
+
+function showWeatherData(data) {
+  let { humidity, pressure } = data.main;
+  let { speed } = data.wind;
+  let { sunrise, sunset } = data.sys;
+
+  timezone.innerHTML = data.timezone;
+  country.innerHTML = data.coord.lat + "N   " + data.coord.lon + "E";
+
+  const timezoneOffset = data.timezone;
+  timezone.innerHTML = timezoneOffset;
+
+  currentWeatherItemsEl.innerHTML = `<div class="weather-item">
+              <div>Humidity</div>
+              <div>${humidity}%</div>
+            </div>
+            <div class="weather-item">
+              <div>Pressure</div>
+              <div>${pressure}</div>
+            </div>
+            <div class="weather-item">
+              <div>Wind Speed</div>
+              <div>${(speed * 3.6).toFixed(2)} km/h</div>
+            </div>
+            <div class="weather-item">
+              <div>Sunrise</div>
+              <div>${window.moment(sunrise * 1000).format("HH.mm a")}</div>
+            </div>
+            <div class="weather-item">
+              <div>Sunset</div>
+              <div>${window.moment(sunset * 1000).format("HH.mm a")}</div>
+            </div>`;
+}
 
 // Alarm Clock
 
